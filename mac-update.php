8fe62f24cb7691be31bef6e59e0ddb057d241bd8
yige <a@wyr.me>
2018-10-18 22:01:14 +0800

    修改获取数据的方式

--- mac-update.php
+++ mac-update.php
@@ -1,21 +1,26 @@
 <?php
-header('Content-Type:application/json; charset=utf-8');
+error_reporting(0);
+require_once 'config.php';
+require_once 'Medoo.php';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://gfe.nvidia.com/mac-update');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 信任任何证书
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 检查证书中是否设置域名
-$output = curl_exec($ch);
+// Using Medoo namespace
+use Medoo\Medoo;
 
-if ($output === false) {
-    echo json_encode([
-        'err' => 'The Nvidia server is not responding:' . curl_error($ch),
-    ]);
+$database = new Medoo($DBCONFIG);
+
+header('Access-Control-Allow-Headers: x-requested-with, content-type, X-Requested-With, Content-Type');
+header('Content-Type: application/json; charset=utf-8');
+
+$datas = $database->select('data', 'content', [
+    'LIMIT' => 1,
+    'ORDER' => [
+        'id' => 'DESC',
+    ],
+]);
+
+if (!empty($datas[0])) {
+    echo $datas[0];
+} else {
+    header('HTTP/1.1 500 Internal Server Error');
+    die('数据错误');
 }
-
-curl_close($ch);
-$xml = simplexml_load_string($output);
-$xmljson = json_encode($xml);
-echo $xmljson;
